31941c1a2da2b66b2414b75008baa92cafe39013
rgowan <ranegowan@gmail.com>
2017-12-24 18:00:15 +0100

    authentication routes fully tested

--- test/controllers/authentication/register.test.js
+++ test/controllers/authentication/register.test.js
@@ -3,7 +3,7 @@ require('../../spec_helper');
 
 const User = require('../../../models/user');
 
-describe('Register routes', () => {
+describe('POST /api/register', () => {
   beforeEach(done => {
     User.collection.remove();
     done();
@@ -14,29 +14,27 @@ describe('Register routes', () => {
     done();
   });
 
-  describe('POST /api/register', () => {
-    it('should register a user providing the correct credentials', done => {
-      api
-        .post('/api/register')
-        .set('Accept', 'application/json')
-        .send({
-          name: {
-            first: 'test',
-            last: 'test'
-          },
-          image: 'http://www.fillmurray.com/300/300',
-          email: 'test@test.com',
-          password: 'password',
-          passwordConfirmation: 'password'
-        })
-        .end((err, res) => {
-          expect(res.status).to.eq(200);
-          expect(res.body).to.be.a('object');
-          expect(res.body.message).to.eq('Welcome');
-          expect(res.body.token).to.be.a('string');
-          done();
-        });
-    });
+  it('should register a user providing the correct credentials', done => {
+    api
+      .post('/api/register')
+      .set('Accept', 'application/json')
+      .send({
+        name: {
+          first: 'test',
+          last: 'test'
+        },
+        image: 'http://www.fillmurray.com/300/300',
+        email: 'test@test.com',
+        password: 'password',
+        passwordConfirmation: 'password'
+      })
+      .end((err, res) => {
+        expect(res.status).to.eq(200);
+        expect(res.body).to.be.a('object');
+        expect(res.body.message).to.eq('Welcome');
+        expect(res.body.token).to.be.a('string');
+        done();
+      });
   });
 
   describe('checking for required user fields', () => {
