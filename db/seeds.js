e3cfd244ad7a8e196354470370f425cd89f6b05c
rgowan <ranegowan@gmail.com>
2017-12-30 17:16:40 +0100

    finished production seed]

--- db/seeds.js
+++ db/seeds.js
@@ -11,77 +11,107 @@ mongoose.connect(db[env], { useMongoClient: true });
 User.collection.drop();
 Language.collection.drop();
 
-console.log(env);
+if (env === 'development') {
+  const globalUsers = [];
 
-// const globalUsers = [];
+  rp('https://randomuser.me/api/?results=25&nat=gb')
+    .then(data => {
+      const { results } = JSON.parse(data);
 
-// rp('https://randomuser.me/api/?results=25&nat=gb')
-//   .then(data => {
-//     const { results } = JSON.parse(data);
+      results.forEach(result => {
+        const user = new User({
+          first: capitalize(result.name.first),
+          last: capitalize(result.name.last),
+          image: result.picture.large,
+          email: `${result.name.first}@${result.name.last}.com`,
+          password: 'password',
+          passwordConfirmation: 'password',
+          online: false
+        });
 
-//     results.forEach(result => {
-//       const user = new User({
-//         first: capitalize(result.name.first),
-//         last: capitalize(result.name.last),
-//         image: result.picture.large,
-//         email: `${result.name.first}@${result.name.last}.com`,
-//         password: 'password',
-//         passwordConfirmation: 'password',
-//         online: false
-//       });
+        globalUsers.push(user);
+        User.create(user);
+      })
+    })
+    .then(() => {
+      return User.create({
+        first: 'Rane',
+        last: 'Gowan',
+        image: 'https://avatars0.githubusercontent.com/u/11501555?s=460&v=4',
+        email: 'rane@gowan.com',
+        password: 'password',
+        passwordConfirmation: 'password',
+        online: false
+      })
+    })
+    .then(user => {
+      globalUsers.push(user);
+      console.log(`${globalUsers.length} users were created!`);
 
-//       globalUsers.push(user);
-//       User.create(user);
-//     })
-//   })
-//   .then(() => {
-//     return User.create({
-//       first: 'Rane',
-//       last: 'Gowan',
-//       image: 'https://avatars0.githubusercontent.com/u/11501555?s=460&v=4',
-//       email: 'rane@gowan.com',
-//       password: 'password',
-//       passwordConfirmation: 'password',
-//       online: false
-//     })
-//   })
-//   .then(user => {
-//     globalUsers.push(user);
-//     console.log(`${globalUsers.length} users were created!`);
+      return Language.create([
+        {name: 'Albanian', code: 'sq', emoji: 'albania'},
+        {name: 'Italian',	code: 'it', emoji: 'it'},
+        {name: 'Arabic', code: 'ar', emoji: 'saudi_arabia'},
+        {name: 'Japanese', code: 'ja', emoji: 'jp'},
+        {name: 'Korean', code: 'ko', emoji: 'kr'},
+        {name: 'Bengali',	code: 'bn', emoji: 'bangladesh'},
+        {name: 'Chinese Simplified', code: 'zh-CN', emoji: 'cn'},
+        {name: 'Chinese Traditional',	code: 'zh-TW', emoji: 'cn'},
+        {name: 'Croatian', code: 'hr', emoji: 'croatia'},
+        {name: 'Czech',	code: 'cs', emoji: 'czech_republic'},
+        {name: 'Polish', code: 'pl', emoji: 'poland'},
+        {name: 'Dutch',	code: 'nl', emoji: 'netherlands'},
+        {name: 'Portuguese', code: 'pt', emoji: 'portugal'},
+        {name: 'Russian', code: 'ru', emoji: 'ru'},
+        {name: 'Serbian', code: 'sr', emoji: 'serbia'},
+        {name: 'French'	, code: 'fr', emoji: 'fr'},
+        {name: 'Spanish' , code: 'es', emoji: 'es'},
+        {name: 'Swedish' , code: 'sv', emoji: 'sweden'},
+        {name: 'German' , code: 'de', emoji: 'de'},
+        {name: 'Greek' , code: 'el', emoji: 'greece'},
+        {name: 'Turkish' , code: 'tr', emoji: 'tr'},
+        {name: 'Hindi' , code: 'hi', emoji: 'india'},
+        {name: 'Urdu'	, code: 'ur', emoji: 'pakistan'},
+      ])
+    })
+    .then(languages => {
+      console.log(`${languages.length} languages were created!`);
+    })
+    .catch(err => console.log(err))
+    .finally(() => mongoose.connection.close());
 
-//     return Language.create([
-//       {name: 'Albanian', code: 'sq', emoji: 'albania'},
-//       {name: 'Italian',	code: 'it', emoji: 'it'},
-//       {name: 'Arabic', code: 'ar', emoji: 'saudi_arabia'},
-//       {name: 'Japanese', code: 'ja', emoji: 'jp'},
-//       {name: 'Korean', code: 'ko', emoji: 'kr'},
-//       {name: 'Bengali',	code: 'bn', emoji: 'bangladesh'},
-//       {name: 'Chinese Simplified', code: 'zh-CN', emoji: 'cn'},
-//       {name: 'Chinese Traditional',	code: 'zh-TW', emoji: 'cn'},
-//       {name: 'Croatian', code: 'hr', emoji: 'croatia'},
-//       {name: 'Czech',	code: 'cs', emoji: 'czech_republic'},
-//       {name: 'Polish', code: 'pl', emoji: 'poland'},
-//       {name: 'Dutch',	code: 'nl', emoji: 'netherlands'},
-//       {name: 'Portuguese', code: 'pt', emoji: 'portugal'},
-//       {name: 'Russian', code: 'ru', emoji: 'ru'},
-//       {name: 'Serbian', code: 'sr', emoji: 'serbia'},
-//       {name: 'French'	, code: 'fr', emoji: 'fr'},
-//       {name: 'Spanish' , code: 'es', emoji: 'es'},
-//       {name: 'Swedish' , code: 'sv', emoji: 'sweden'},
-//       {name: 'German' , code: 'de', emoji: 'de'},
-//       {name: 'Greek' , code: 'el', emoji: 'greece'},
-//       {name: 'Turkish' , code: 'tr', emoji: 'tr'},
-//       {name: 'Hindi' , code: 'hi', emoji: 'india'},
-//       {name: 'Urdu'	, code: 'ur', emoji: 'pakistan'},
-//     ])
-//   })
-//   .then(languages => {
-//     console.log(`${languages.length} languages were created!`);
-//   })
-//   .catch(err => console.log(err))
-//   .finally(() => mongoose.connection.close());
-
-
-// function capitalize(name) {
-//   return name.charAt(0).toUpperCase() + name.slice(1);
-// }
+  function capitalize(name) {
+    return name.charAt(0).toUpperCase() + name.slice(1);
+  }
+} else {
+  Language.create([
+    {name: 'Albanian', code: 'sq', emoji: 'albania'},
+    {name: 'Italian',	code: 'it', emoji: 'it'},
+    {name: 'Arabic', code: 'ar', emoji: 'saudi_arabia'},
+    {name: 'Japanese', code: 'ja', emoji: 'jp'},
+    {name: 'Korean', code: 'ko', emoji: 'kr'},
+    {name: 'Bengali',	code: 'bn', emoji: 'bangladesh'},
+    {name: 'Chinese Simplified', code: 'zh-CN', emoji: 'cn'},
+    {name: 'Chinese Traditional',	code: 'zh-TW', emoji: 'cn'},
+    {name: 'Croatian', code: 'hr', emoji: 'croatia'},
+    {name: 'Czech',	code: 'cs', emoji: 'czech_republic'},
+    {name: 'Polish', code: 'pl', emoji: 'poland'},
+    {name: 'Dutch',	code: 'nl', emoji: 'netherlands'},
+    {name: 'Portuguese', code: 'pt', emoji: 'portugal'},
+    {name: 'Russian', code: 'ru', emoji: 'ru'},
+    {name: 'Serbian', code: 'sr', emoji: 'serbia'},
+    {name: 'French'	, code: 'fr', emoji: 'fr'},
+    {name: 'Spanish' , code: 'es', emoji: 'es'},
+    {name: 'Swedish' , code: 'sv', emoji: 'sweden'},
+    {name: 'German' , code: 'de', emoji: 'de'},
+    {name: 'Greek' , code: 'el', emoji: 'greece'},
+    {name: 'Turkish' , code: 'tr', emoji: 'tr'},
+    {name: 'Hindi' , code: 'hi', emoji: 'india'},
+    {name: 'Urdu'	, code: 'ur', emoji: 'pakistan'},
+  ])
+  .then(languages => {
+    console.log(`${languages.length} languages were created!`); 
+  })
+  .catch(err => console.log(err))
+  .finally(() => mongoose.connection.close());
+}
